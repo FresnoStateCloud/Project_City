6d1cc6c11abcc67de1e7e64554268f3f18c5456a
unknown <DXI@TS-DXI-L003.ad.csufresno.edu>
2022-08-16 11:45:42 -0700

    # This is a combination of 2 commits. # This is the 1st commit message:
    iOS compatibiltiy fixup
    
    # The commit message #2 will be skipped:
    
    # iPhone testing compat version

--- scripts/touchscreen-listener.js
+++ scripts/touchscreen-listener.js
@@ -1,18 +1,19 @@
 AFRAME.registerComponent('touch-listener', {
-    init: function () {
-      var targetEl = this.el;
-      
-      //Create the touchstart event
-      document.querySelector('a-scene').addEventListener('touchstart', function () {
-   //Emit this event to listen it from the teleport controls
-        targetEl.emit('startouch')
-      })
-      
-      //Create the touchend event
-       document.querySelector('a-scene').addEventListener('touchend', function () {
-  //Emit this event to listen it from the teleport controls
-          targetEl.emit('clickend')
-      })
-      
-    }
+  init: function () {
+    var targetEl = this.el;
+
+    //Create the touchstart event
+    document
+      .querySelector('a-scene')
+      .addEventListener('touchstart', function () {
+        //Emit this event to listen it from the teleport controls
+        targetEl.emit('startouch');
+      });
+
+    //Create the touchend event
+    document.querySelector('a-scene').addEventListener('touchend', function () {
+      //Emit this event to listen it from the teleport controls
+      targetEl.emit('clickend');
+    });
+  }
 });
